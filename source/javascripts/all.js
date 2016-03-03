324e9b00607c8da724ea28c41125e8cfbd41a6e4
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-03-03 14:18:48 +0530

    wrapup

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,68 @@
-// This is where it all goes :)
+$(function() {
+  "use strict";
+
+  //underscore template settings
+  _.templateSettings = {
+    interpolate: /\{\{(.+?)\}\}/g
+  };
+
+  var sheetURLPrefix = "https://googlesheets.zammu.in/sheet/ReAVzqzlmilw7bg7z7GnzA/",
+    paymentID = window.location.hash.replace("#", "");
+
+  //ba93b6b9-a193-45a6-bb3f-d34f7c694bff
+  if (paymentID.length != 36) {
+    // TODO: use sweet alert here
+    alert("Invalid url");
+  }
+
+  var tmpl = _.template($("#tmpl").html())
+    // get payment and render our template
+  $.getJSON(sheetURLPrefix + paymentID + "?" + (new Date()).getTime(), function(resp) {
+    console.log("Got", resp)
+
+    var html = tmpl({
+      payment_id: paymentID,
+      short_payment_id: paymentID.split("-")[0],
+      customer_name: resp.customer_name,
+      amount: resp.amount,
+      amount_in_paise: resp.amount * 100,
+      description: resp.description,
+    });
+
+    html = $.parseHTML(html, document, true)
+    $("#out").append(html)
+
+
+    var rzp = new Razorpay(paymentOptionsFor(resp));
+
+    $("#rzp").click(function(e) {
+      rzp.open();
+      e.preventDefault();
+    });
+
+  })
+
+  function paymentOptionsFor(payment) {
+    return {
+      "key": "rzp_live_QWpjLoRplrGOaD",
+      "amount": payment.amount * 100,
+      "name": "Cosmicvent Software",
+      "description": payment.description,
+      "image": "/images/cosmicvent-logo-optimized.png",
+      "handler": function(response) {
+        console.log("<<>>", response.razorpay_payment_id);
+      },
+      "prefill": {
+        "email": "payments@cosmicvent.com"
+      },
+      //"notes": {
+      //"address": "Hello World"
+      //},
+      "theme": {
+        "color": "#F37254"
+      }
+    }
+
+  }
+
+})
